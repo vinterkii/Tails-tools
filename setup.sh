515c586c3f8c46ce85f8dd650bef452c3e3192dc
vinterkii <duckerkii@duck.coms>
2026-07-16 22:07:41 +0000

    added a reinstall, update & remove functionality and some cleanup

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# this is a script that turns the scripts in 'Scripts' into bash/zsh commands
+# this is a script that turns the scripts in 'Scripts' into shell commands
 
 # ANSI Color & Style
 RED='\033[0;31m'
@@ -12,14 +12,16 @@ ITALIC='\x1B[3m'
 
 RESET='\033[0m\x1B[0m'
 
+# Geting the Tails-tools Directory
 DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 
-# get the config file for the shell
+# Get The Path to the used shell configuration
 case $SHELL in 
     */bash)  SHELL_CONFIG="${HOME}/.bashrc" ;;
     */zsh)  SHELL_CONFIG='$HOME/.zshrc' ;;
     */fish)  SHELL_CONFIG='$HOME/.config/fish/config.fish' ;;
     */dash)   SHELL_CONFIG='$HOME/.profile' ;;
+    # Support for more shells can be added by just adding more cases 
     *) 
         echo -e "${RED} Sorry Your Shell is not supported yet...${RESET}" 
         echo -e "${YELLOW} Please Use (Bash, Zsh, Fish or dash)${RESET}" 
@@ -27,54 +29,131 @@ case $SHELL in
         exit ;;
 esac
 
-install() {
+# the backup function runs before every function that changes the Shell Config
+backup_config() {
+    # Set Backup Date & Time
     local date
-    date="$(date '+%Y-%m-%d')"
+    date="$(date '+%Y-%m-%d|%H:%M:%S')"
 
-    # Backups the Shell Config (e.g. .bashrc ...)
+    # Set Backup Path
+    if grep -qi 'tails' /etc/os-release ;then
+        if [[ -d "/live/persistence/TailsData_unlocked/dotfiles/Backup/Shell_Config" ]] ;then
+            SHELL_CONFIG_BACKUP="/live/persistence/TailsData_unlocked/dotfiles/Backup/Shell_Config/${SHELL##/*/}_${date}_Backup.txt"
+        else # if the Persistent Backup folder Hasn't been setup use the normal folder and the user will be prompted later to set it up
+            SHELL_CONFIG_BACKUP="$HOME/Backup/Shell_Config/${SHELL##/*/}_${date}_Backup.txt"
+        fi
+    else # If not using tailsOS just continue as normal
+        SHELL_CONFIG_BACKUP="$HOME/Backup/Shell_Config/${SHELL##/*/}_${date}_Backup.txt"
+    fi
+    # Make the Backup Directory
     mkdir -p "$HOME/Backup/Shell_Config"
-    cat "$SHELL_CONFIG" >> "$HOME/Backup/Shell_Config/${SHELL##/*/}_${date}_Backup.txt"
-    echo -e "${YELLOW}Your '${SHELL_CONFIG}' Have been backuped in to '$HOME/Backup/Shell_Config/${SHELL##/*/}_${date}_Backup.txt' ${RESET}"
+    # Make a backup
+    cat "$SHELL_CONFIG" >> "$SHELL_CONFIG_BACKUP"
+    echo -e "${YELLOW}Your '$SHELL_CONFIG' Have been backed up in to '$SHELL_CONFIG_BACKUP' ${RESET}"
+    return
+}
 
-    cat >> "$SHELL_CONFIG" <<EOF
+# The install function
+install() { #This function can't use indentation becuse Bash Scripting Sucks
+
+# Backups For Safety
+backup_config
+
+cat >> "$SHELL_CONFIG" <<EOF # Apends thee Following text to SHELL_CONFIG
 # Tails-tools setup MlVkT0a3
-alias wall="bash ${DIR}/Scripts/wall.sh"
-
+wall() {
+    bash ${DIR}/Scripts/wall.sh $1
+}
 # End Of Tails-tools (PS: Don't Add any thing inside it will be replaced when reinstalling)
 
 EOF
-
-  echo -e "${GREEN}Done${RESET}"
+echo -e "${GREEN}Done.${RESET} Tails-tools is now installed"
+refresh
+return
 }
 
+# The remove function
+uninstall() {
+    local start_marker="# Tails-tools setup MlVkT0a3"
+    local end_marker="# End Of Tails-tools"
+    
+    # Rechecks if setup.sh has run before
+    if grep -qF "$start_marker" "$SHELL_CONFIG"; then
+        backup_config
+        CONDITION=$()
+        if readlink $SHELL_CONFIG ;then
+            local SHELL_CONFIG_TAILSOS
+            SHELL_CONFIG_TAILSOS=$(readlink $SHELL_CONFIG)
+            # remove every line that lies between the start_marker and end_marker
+            sed -i "/$start_marker/,/$end_marker/d" "$SHELL_CONFIG_TAILSOS"
+            echo -e "${GREEN} Done.${RESET} Tails tools have been removed."
+        else
+            # remove every line that lies between the start_marker and end_marker
+            sed -i "/$start_marker/,/$end_marker/d" "$SHELL_CONFIG"
+            echo -e "${GREEN} Done.${RESET} Tails tools have been removed."
+        fi
+    else # This Probebly Wont be the case ever (Unless somthing changes while the script is running)
+        echo -e "${RED}Tails-tools isn't installed or have been messed with.${RESET}"
+        echo -e "Automatic removal is not available"
+    fi
+    refresh
+    return
+}
+
+# Update function using git
+update() {
+    if which git;then
+        if [[ -d ".git" ]]; then
+            git pull origin main
+        else
+            git init
+            git remote set-url origin https://github.com/vinterkii/Tails-tools
+            git pull origin main
+        fi
+        echo -e "${YELLOW}Please Run the setup agian and select 'reinstall'${RESET}"
+        return
+    else
+        echo -e "${RED}Git is not installed.... somehow???"
+        return
+    fi 
+}
 
 reinstall() {
-    echo "WIP"
+    echo -e "${YELLOW}Tails-tools will be reinstalled soon..."
+    uninstall
+    sleep .5
+    install
+    echo -e "${GREEN}Done.${RESET} Tails-tools has been re installed"
     refresh
+    return
 }
 
+# this function lets the changes to SHELL_CONFIG apply to the current terminal without restaritng it
 refresh() {
     case "$SHELL" in
         */bash) source ${HOME}/.bashrc ;;
         */zsh) source $HOME/.zshrc ;;
         */fish) source $HOME/.config/fish/config.fish ;;
         */dash)  . $HOME/.profile ;;
-        *) echo -e "${RED} Sorry Your Shell is not supported yet... Please Use (Bash, Zsh, Fish or dash)${RESET}" ;;
+        *) echo -e "${YELLOW}Please restart your terminal to apply changes..${RESET}" ;;
     esac
+    return
 }
 
 # Check if the setup has run before
 if grep -q "# Tails-tools setup MlVkT0a3" $SHELL_CONFIG ; then
     echo -e "The Script Has been Setup Before..."
-    echo ""
-    read -p "Would You want to reinstall? (y/n):" ACTION
-    case "$ACTION" in
-        y|Y|yes)
-            reinstall ;;
-        n|N|no|"")
-            echo -e "Reinstall Canceled..."
-            echo -e "if you're Having Problems running the commands try restartig your terminal"
-            refresh ;;
+    # Choices
+    echo -e "What do you want to do:"
+    echo -e "${YELLOW} 1. ${RESET}reinstall"
+    echo -e "${YELLOW} 2. ${RESET}update"
+    echo -e "${YELLOW} 3. ${RESET}uninstall"
+    read -p "Please Choose an action: " ACTION
+    case $ACTION in
+        1|reinstall) reinstall;;
+        2|update) update && echo -e "${YELLOW}Please Run the setup agian and select 'reinstall'${RESET}";;
+        3|remove) uninstall;;
+        *) refresh ;;
     esac
 else
     echo -e "The script will modify your ${SHELL_CONFIG}"
